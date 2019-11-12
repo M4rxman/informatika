22e27608e01de8d3d284460d4474c8e3970c1ef0
Alex <53474466+M4rxman@users.noreply.github.com>
2019-11-12 13:04:40 +0200

    nomer 6.40

---
+++ 6.40gg.pas
@@ -0,0 +1,15 @@
+program 6.40gg ;
+var i:real , k, n, a, :integer;
+ begin
+  WRITELN('k=');
+  READLN(k);
+  WRITELN('a=');
+  READLN(a);
+  k := 2*k-1
+  a :2*a+1
+  while k=<21 DO
+   begin
+   n:= k * a
+   end;
+  write(n);
+  end.
