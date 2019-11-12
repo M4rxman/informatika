134bde721de07a1f8ed045a4d803a3c456710ddb
Alex <53474466+M4rxman@users.noreply.github.com>
2019-11-12 13:58:21 +0200

    Add files via upload

--- 6.39gg.pas
+++ 6.39gg.pas
@@ -1,14 +1,15 @@
 program gg ;
-var i, n, k:real; a, b:integer;
+var p:real; 
+i:integer;
  begin
-  writeln('n=');
-  read(n);
+
   i:= 1;
-  a:= 1;
-  while i>0.006 DO
-   begin
-   i:= a/n ;
-   n:=n+1 ;
-   write(i);
+  p:= 1;
+  while 1/i>0.006 DO
+   begin   
+   p:=p*(1/i) ;
+   i:= i+1 ;
+   
    end;
+   write(p);
  end.
