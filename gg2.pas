dac613b89ab2010ab0769ffc61793ec9853f4082
Alex <53474466+M4rxman@users.noreply.github.com>
2019-11-19 13:33:21 +0200

    pascal2

---
+++ gg2.pas
@@ -0,0 +1,14 @@
+program ggb;
+var p, x:real; k, n:integer;
+BEGIN  
+ writeln('n=');
+ readln(n);
+ writeln('x=');
+ readln(x);
+ p:=1 ;
+  for k:=1 to n do
+    begin 
+    p:=p*((2*k+1)*x)/(k+1) ;
+    end;
+  writeln(p);
+  end.
